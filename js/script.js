d1796d733e66ddd30fda0d4c4cd03aaa176f00bd
Pedro-Augusto-dev <pedroqueiroz357@gmail.com>
2024-04-06 20:08:02 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -32,3 +32,47 @@ function somar() {
     }
 
 }
+
+function ascender() {
+
+    var light = document.getElementById("day");
+    light.style.display = "none"
+
+    var dark = document.getElementById("night")
+    dark.style.display = "flex"
+
+    var background = document.getElementById("bacColor")
+    background.style.backgroundColor = "rgb(239, 235, 228)"
+
+    var text = document.getElementById("mb")
+    text.style.color = "black"
+
+    var back = document.getElementById("clicar");
+    back.style.backgroundColor = "#2e2e2e"
+
+    var micText = document.getElementById("micro")
+    micText.style.color = "black"
+}
+
+function escurecer(){
+
+    var light = document.getElementById("night");
+    light.style.display = "none"
+
+    var dark = document.getElementById("day")
+    dark.style.display = "flex"
+
+    var background = document.getElementById("bacColor")
+    background.style.backgroundColor = "#2b2b2b"
+
+    var text = document.getElementById("mb")
+    text.style.color = "white"
+
+    var back = document.getElementById("clicar");
+    back.style.backgroundColor = "#a5a5a5"
+
+    var micText = document.getElementById("micro")
+    micText.style.color = "white"
+
+
+}
